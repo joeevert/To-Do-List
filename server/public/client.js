9c20e037999361333592e45f3a6fc180a5abc0b2
Joe Evert <joeevert@gmail.com>
2018-10-20 11:21:53 -0500

    started get on client.js

--- server/public/client.js
+++ server/public/client.js
@@ -4,4 +4,29 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('JQ');
+    clickListeners();
+}
+
+function clickListeners() {
+    $('#add-btn').on('click', addTask)
+} // end clickListeners
+
+function addTask() {
+    event.preventDefault();
+    console.log('in addTask');   
+} // end addTask
+
+function getList() {
+    console.log('in getList');
+    
+    $.ajax({
+        method: 'GET',
+        url: '/list'
+    })
+} // end getList
+
+function displayList() {
+    console.log('in displayList');
+    
+
 }
