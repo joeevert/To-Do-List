5cc172d8fd11fcbbc0edb706f494311571d7edf9
Joe Evert <joeevert@gmail.com>
2018-10-20 10:55:02 -0500

    server get

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const listRouter = require('./routes/list.router.js');
 
 const app = express();
 
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use( bodyParser.json() );
+
+app.use( '/list', listRouter);
 
 app.use( express.static('server/public') );
 
