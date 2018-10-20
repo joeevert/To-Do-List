a9f0d933ca8e4b54134eb290fd994a7458349187
Joe Evert <joeevert@gmail.com>
2018-10-20 12:43:41 -0500

    get route client

--- server/public/client.js
+++ server/public/client.js
@@ -11,11 +11,13 @@ function clickListeners() {
     $('#add-btn').on('click', addTask)
 } // end clickListeners
 
+// POST
 function addTask() {
     event.preventDefault();
     console.log('in addTask');   
 } // end addTask
 
+// GET
 function getList() {
     console.log('in getList');
     
@@ -23,10 +25,25 @@ function getList() {
         method: 'GET',
         url: '/list'
     })
+    .then( function (response) {
+        console.log(`response: ${response}`);
+        displayList(response);
+    })
+    .catch( function (error) {
+        console.log(`error getting list: ${error}`);   
+    })
 } // end getList
 
-function displayList() {
+function displayList(list) {
     console.log('in displayList');
-    
-
-}
+    $('#todo-list').empty();
+    for( let task of list) {
+        let element = `
+        <tr>
+            <td>${task.name}</td>
+            <td><button id="edit-btn">Edit</button></td>
+            <td><button id="remove-btn">Remove</button></td>
+        </tr>`
+        $('#todo-list').append(element);
+    } // end for of
+} // end displayList
